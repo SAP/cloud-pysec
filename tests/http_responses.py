a10f1c1fd0ccbb39c347176c8ec03916a82f5039
Patrick Firnkes <patrick.firnkes@sap.com>
2019-05-28 08:15:52 +0000

    Avoid usage of internals in tests

--- tests/http_responses.py
+++ tests/http_responses.py
@@ -39,3 +39,33 @@ HTTP_SUCCESS = json.loads(r'''{
 }''' % (KEY_ID_0, KEY_ID_1), strict=False)
 
 
+HTTP_SUCCESS_DUMMY = json.loads(r'''{
+    "keys": [
+        {
+            "kty": "RSA",
+            "e": "AQAB",
+            "use": "sig",
+            "kid": "key-id-0",
+            "alg": "RS256",
+            "value": "dummy-key",
+            "n": "AJjTNzl32UtFLvHmGVwoBlhYFVkF-jB52nWJN8x2eTyD3g2NwKWkhqTBIlcJ9XE-ilFRzCx3Js9YLDcu
+            -KQp5gmttluydwaGbpc0dAN-2sjFa0R4d5334MkpPLufNZdNm723KWm93txKLUjeS4sRk9VVmbw22pV3-p-ZKuOfTVi
+            -mc5BLNtDKzhJOXC3Z7IoE0FB0iiEOU6ZXcg5CTJts8DpawdkffOPkHZQxZqFR-2Gro8a9oNGferu1vSJopOsE4hXPFu3lF34Txp
+            -63lS6tf-aNjc9CcdHoxRw8Exp3LPpNUQUug26UzjK_bZCRHN2bF9xbeDragpEVyOYVJmvh8"
+        },
+        {
+            "kty": "RSA",
+            "e": "AQAB",
+            "use": "sig",
+            "kid": "key-id-1",
+            "alg": "RS256",
+            "value": "dummy-key",
+            "n": "AJjTNzl32UtFLvHmGVwoBlhYFVkF-jB52nWJN8x2eTyD3g2NwKWkhqTBIlcJ9XE-ilFRzCx3Js9YLDcu
+            -KQp5gmttluydwaGbpc0dAN-2sjFa0R4d5334MkpPLufNZdNm723KWm93txKLUjeS4sRk9VVmbw22pV3-p-ZKuOfTVi
+            -mc5BLNtDKzhJOXC3Z7IoE0FB0iiEOU6ZXcg5CTJts8DpawdkffOPkHZQxZqFR-2Gro8a9oNGferu1vSJopOsE4hXPFu3lF34Txp
+            -63lS6tf-aNjc9CcdHoxRw8Exp3LPpNUQUug26UzjK_bZCRHN2bF9xbeDragpEVyOYVJmvh8"
+        }
+    ]
+}''', strict=False)
+
+
--- tests/test_key_cache.py
+++ tests/test_key_cache.py
@@ -28,74 +28,80 @@ class CacheTest(unittest.TestCase):
 
     def test_not_hit_load_key(self, mock_requests, mock_time):
         mock_requests.return_value = self.mock
-        self.mock.json.return_value = HTTP_SUCCESS
-        self.cache._cache[KeyCache._create_cache_key("jku2", "key-id-1")] = CacheEntry("dummy-key", MOCKED_CURRENT_TIME)
+        self.mock.json.side_effect = [HTTP_SUCCESS_DUMMY, HTTP_SUCCESS]
+        self.cache.load_key("jku2", "key-id-1")
 
         key = self.cache.load_key("jku1", "key-id-1")
 
         self.assert_key_equal(KEY_ID_1, key)
-        mock_requests.assert_called_once_with("jku1", timeout=constants.HTTP_TIMEOUT_IN_SECONDS)
+        self.assertEqual(2, mock_requests.call_count)
 
     def test_hit_do_not_load_key(self, mock_requests, mock_time):
         mock_requests.return_value = self.mock
-        self.mock.json.return_value = HTTP_SUCCESS
-        self.cache._cache[KeyCache._create_cache_key("jku1", "key-id-1")] = CacheEntry("dummy-key", MOCKED_CURRENT_TIME -
-                                                                                       (constants.KEYCACHE_DEFAULT_CACHE_ENTRY_EXPIRATION_TIME_IN_MINUTES - 1) * 60)
+        self.mock.json.side_effect = [HTTP_SUCCESS_DUMMY, HTTP_SUCCESS]
+
+        mock_time.return_value = MOCKED_CURRENT_TIME - (constants.KEYCACHE_DEFAULT_CACHE_ENTRY_EXPIRATION_TIME_IN_MINUTES - 1) * 60
+        self.cache.load_key("jku1", "key-id-1")
+        mock_time.return_value = MOCKED_CURRENT_TIME
 
         key = self.cache.load_key("jku1", "key-id-1")
 
         self.assert_key_equal("dummy-key", key)
-        self.assertFalse(mock_requests.called)
+        self.assertEqual(1, mock_requests.call_count)
 
     def test_expired_key_load_key(self, mock_requests, mock_time):
         mock_requests.return_value = self.mock
-        self.mock.json.return_value = HTTP_SUCCESS
-        self.cache._cache[KeyCache._create_cache_key("jku1", "key-id-1")] = CacheEntry("dummy-key",
-                                                                                       MOCKED_CURRENT_TIME -
-                                                                                       (constants.KEYCACHE_DEFAULT_CACHE_ENTRY_EXPIRATION_TIME_IN_MINUTES + 1) * 60)
+        self.mock.json.side_effect = [HTTP_SUCCESS_DUMMY, HTTP_SUCCESS]
+
+        mock_time.return_value = MOCKED_CURRENT_TIME - (constants.KEYCACHE_DEFAULT_CACHE_ENTRY_EXPIRATION_TIME_IN_MINUTES + 1) * 60
+        self.cache.load_key("jku1", "key-id-1")
+        mock_time.return_value = MOCKED_CURRENT_TIME
 
         key = self.cache.load_key("jku1", "key-id-1")
 
         self.assert_key_equal(KEY_ID_1, key)
-        mock_requests.assert_called_once_with("jku1", timeout=constants.HTTP_TIMEOUT_IN_SECONDS)
+        self.assertEqual(2, mock_requests.call_count)
 
     def test_kid_does_not_match(self, mock_requests, mock_time):
         mock_requests.return_value = self.mock
-        self.mock.json.return_value = HTTP_SUCCESS
-        self.cache._cache[KeyCache._create_cache_key("jku2", "key-id-1")] = CacheEntry("dummy-key", MOCKED_CURRENT_TIME)
+        self.mock.json.side_effect = [HTTP_SUCCESS_DUMMY, HTTP_SUCCESS]
+        self.cache.load_key("jku2", "key-id-1")
 
         key = self.cache.load_key("jku2", "key-id-0")
 
         self.assert_key_equal(KEY_ID_0, key)
-        mock_requests.assert_called_once_with("jku2", timeout=constants.HTTP_TIMEOUT_IN_SECONDS)
+        self.assertEqual(2, mock_requests.call_count)
 
     def test_cache_max_size(self, mock_requests, mock_time):
         mock_requests.return_value = self.mock
-        self.mock.json.return_value = HTTP_SUCCESS
+        self.mock.json.return_value = HTTP_SUCCESS_DUMMY
         for i in range(0, constants.KEYCACHE_DEFAULT_CACHE_SIZE):
-            self.cache._cache[KeyCache._create_cache_key("jku-" + str(i), "key-id-1")] = CacheEntry("dummy-key", MOCKED_CURRENT_TIME)
+            self.cache.load_key("jku-" + str(i), "key-id-1")
+
         self.assertEqual(len(self.cache._cache), constants.KEYCACHE_DEFAULT_CACHE_SIZE)
         self.assertTrue(KeyCache._create_cache_key("jku-0", "key-id-1") in self.cache._cache)
 
+        self.mock.json.return_value = HTTP_SUCCESS
         key = self.cache.load_key("jku1", "key-id-0")
 
         self.assert_key_equal(KEY_ID_0, key)
-        mock_requests.assert_called_once_with("jku1", timeout=constants.HTTP_TIMEOUT_IN_SECONDS)
+        self.assertEqual(constants.KEYCACHE_DEFAULT_CACHE_SIZE + 1, mock_requests.call_count)
         self.assertEqual(len(self.cache._cache), constants.KEYCACHE_DEFAULT_CACHE_SIZE)
         # assert that least recently inserted key got deleted
         self.assertFalse(KeyCache._create_cache_key("jku-0", "key-id-1") in self.cache._cache)
 
     def test_update_increases_insertion_order(self, mock_requests, mock_time):
         mock_requests.return_value = self.mock
-        self.mock.json.return_value = HTTP_SUCCESS
+        self.mock.json.return_value = HTTP_SUCCESS_DUMMY
         for i in range(0, constants.KEYCACHE_DEFAULT_CACHE_SIZE):
-            self.cache._cache[KeyCache._create_cache_key("jku-" + str(i), "key-id-1")] = CacheEntry("dummy-key", MOCKED_CURRENT_TIME)
+            self.cache.load_key("jku-" + str(i), "key-id-1")
         self.assertEqual(len(self.cache._cache), constants.KEYCACHE_DEFAULT_CACHE_SIZE)
         self.assertTrue(KeyCache._create_cache_key("jku-0", "key-id-1") in self.cache._cache)
 
         # first cache entry is invalid -> must be updated
         self.cache._cache[KeyCache._create_cache_key("jku-0", "key-id-1")].insert_timestamp = 0
 
+        self.mock.json.return_value = HTTP_SUCCESS
         # update first cache entry -> should not deleted if new key is added
         self.cache.load_key("jku-0", "key-id-1")
         self.assertTrue(KeyCache._create_cache_key("jku-0", "key-id-1") in self.cache._cache)
