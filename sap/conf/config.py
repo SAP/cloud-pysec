adf9aa6d43e70b615e6a211d6c9eee7ef321bce8
Patrick Firnkes <patrick.firnkes@sap.com>
2019-06-03 10:06:10 +0000

    Fix configparser dependency

--- sap/conf/config.py
+++ sap/conf/config.py
@@ -1,7 +1,11 @@
-import configparser
 import os
 
-config = configparser.ConfigParser()
+try:
+    from configparser import ConfigParser
+except ImportError:
+    from ConfigParser import SafeConfigParser as ConfigParser
+
+config = ConfigParser()
 config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', '..', 'sap_xssec.ini'))
 
 USE_SAP_PY_JWT = config.getboolean('DEFAULT', 'use_sap_py_jwt')
