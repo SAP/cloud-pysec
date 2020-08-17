215d27f8bb08fa987e8972021a37b42b8d2bc5e9
I068828 <shruthi.annappa@sap.com>
2020-08-17 12:24:07 +0530

    Adding audience validation check

--- sap/xssec/security_context.py
+++ sap/xssec/security_context.py
@@ -326,6 +326,8 @@ class SecurityContext(object):
         if(self._config['xsappname']):
             audience_validator.configure_trusted_clientId(self._config['xsappname'])
         validation_result = audience_validator.validate_token(self.get_clientid(), self.get_audience(), self._properties['scopes'])   
+        if validation_result is False:
+                raise RuntimeError('Audience Validation Failed')
 
 
     def _get_property_of(self, property_name, obj):
