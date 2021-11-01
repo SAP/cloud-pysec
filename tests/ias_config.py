002537dc634f03eff43fcd3a548dcbd70f45c063
Allen Liu <allen.liu05@sap.com>
2021-11-01 08:42:26 +0800

    update

---
+++ tests/ias_config.py
@@ -0,0 +1,9 @@
+VALID = {
+    "ias": {
+        "clientid": "clientid",
+        "clientsecret": "SECRET",
+        "domain": "accounts400.ondemand.com",
+        "url": "https://tenant.accounts400.ondemand.com",
+        "zone_uuid": "1c3bc4e1-0d22-4497-b7bb-06c5f1494d79"
+    }
+}
