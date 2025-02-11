e35ee842dd666dc5e55276d437e0443ff4cd716c
Allen Liu <allen.liu05@sap.com>
2025-02-11 15:06:06 +0800

    Allow overriding HTTP timeout using environment variable resolves #72

---
+++ tests/test_config.py
@@ -0,0 +1,13 @@
+from sap.xssec import constants
+from importlib import reload
+
+
+def test_http_timeout_overridden(monkeypatch):
+    monkeypatch.setenv("XSSEC_HTTP_TIMEOUT_IN_SECONDS", "10")
+    reload(constants)
+    assert constants.HTTP_TIMEOUT_IN_SECONDS == 10
+
+    monkeypatch.delenv("XSSEC_HTTP_TIMEOUT_IN_SECONDS")
+    reload(constants)
+    assert constants.HTTP_TIMEOUT_IN_SECONDS == 2
+
