9efad7f06b7b3a98aac1f4088e1a922c7c70a3ba
Allen Liu <allen.liu05@sap.com>
2025-02-11 15:10:46 +0800

    update

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,6 +3,8 @@ from importlib import reload
 
 
 def test_http_timeout_overridden(monkeypatch):
+    assert constants.HTTP_TIMEOUT_IN_SECONDS == 2
+
     monkeypatch.setenv("XSSEC_HTTP_TIMEOUT_IN_SECONDS", "10")
     reload(constants)
     assert constants.HTTP_TIMEOUT_IN_SECONDS == 10
