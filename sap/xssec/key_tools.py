f0528d4ec5f49e791b13ea8051d967e34553b315
Pr Janitor <prjanitortool@gmail.com>
2026-04-04 14:17:30 +0300

    Fix incorrect padding added to JWK modulus
    Remove the line that unconditionally appends '==' to the JWK modulus (n) parameter. This was corrupting the base64url-encoded value and causing RSA key reconstruction to fail. The PyJWT library handles proper base64url decoding internally.

--- sap/xssec/key_tools.py
+++ sap/xssec/key_tools.py
@@ -5,7 +5,6 @@ from jwt.algorithms import RSAAlgorithm
 
 
 def jwk_to_pem(jwk) -> str:
-    jwk["n"] = jwk["n"] + "=="  # avoid `incorrect padding`
     pubkey = RSAAlgorithm.from_jwk(json.dumps(jwk))
     pem = pubkey.public_bytes(Encoding.PEM, PublicFormat.SubjectPublicKeyInfo).decode()
     return pem
